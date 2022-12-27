c2b44f076044492cae15d17cfc71f578595719bb
PRAJWAL H S <48756698+PRAJWAL7HS@users.noreply.github.com>
2022-12-27 12:43:19 +0530

    Update channels.py

--- video_cms/channels.py
+++ video_cms/channels.py
@@ -1,57 +1,65 @@
 import utils
-import time
-        
-def channelAdd(decodedBody,connection):
+
+def channelAdd(userId,decodedBody,connection):
     print("Received channelAdd request")
     
     cursor = connection.cursor()  # get the cursor object
     
     channel_name = decodedBody["channel_name"]
-    channel_Description = decodedBody["channel_Description"]
+    channel_description = decodedBody["channel_Description"]
     channel_profile_pic = decodedBody["channel_profile_pic"]
-    created_at = decodedBody["created_at"]
-    channel_user_id = decodedBody["channel_user_id"]
-    sql="""SELECT EXISTS(SELECT * FROM channel WHERE channel_name = %s)"""
-    cursor.execute(sql,(channel_name,))
-    extracted_data = cursor.fetchone()
+    channel_user_id = userId
+    category = decodedBody["category"]
+
     
-    if extracted_data[0]==0:
-        sql = "INSERT INTO channel (channel_name,channel_Description,channel_profile_pic,created_at,channel_user_id) VALUES (%s,%s,%s,%s,%s)"
-        val = (channel_name,channel_Description,channel_profile_pic,created_at,channel_user_id)
+    
+    result=[]
+    fields=['id','channel_name','channel_description','channel_profile_pic','channel_user_id','created_at','category']
+
+    sql="""SELECT EXISTS(SELECT * FROM users where id=%s)"""
+    cursor.execute(sql,(userId,))
+    extracted_data=cursor.fetchone()
+    print(extracted_data[0],len(extracted_data))
+    
+    if extracted_data[0]!=0:
+      sql="""SELECT * FROM channel where channel_name=%s"""
+      cursor.execute(sql,(channel_name,))
+      extracted_data_c=cursor.fetchall()
+      print(extracted_data_c,len(extracted_data_c))
+    
+      if(len(extracted_data_c)==0):
+        sql="""INSERT into channel(channel_name,channel_Description,channel_profile_pic,channel_user_id,category) value(%s,%s,%s,%s,%s)"""
+        val=(channel_name,channel_description,channel_profile_pic,channel_user_id,category)
         cursor.execute(sql,val)
+      
         connection.commit()
-    
-    else :
         
+        sql="""Select id,channel_name,channel_description,channel_profile_pic,channel_user_id,created_at,category from channel where channel_name=%s"""
+        cursor.execute(sql,(channel_name,))
+        extracted_data_ch=cursor.fetchall()
+        for row in extracted_data_ch:
+          response_da=dict(zip(fields,row))
+          result.append(response_da)
+        response_data={'message':'Channel Added',
+                      'data':result}
+        cursor.close()
+        #return http response 
+        #return utils.response("Passed",response_data)
+      
+      else:
+        sql="""Select id,channel_name,channel_description,channel_profile_pic,channel_user_id,created_at,category from channel where channel_name=%s"""
+        cursor.execute(sql,(channel_name,))
+        extracted_data_ch=cursor.fetchall()
+        for row in extracted_data_ch:
+          response_da=dict(zip(fields,row))
+          result.append(response_da)
+        response_data={'message':'Channel already exist for user',
+                      'data':result}
         cursor.close()
         
-        response_data={'message':'channel name already exist',
-                        'data':[]}
-        
-        #return http response 
-        return utils.response("Failed",response_data)
+    else:
+      cursor.close()
+      response_data={'message':'user not exist','data':[]}
+    return utils.response("Passed",response_data)
     
     
-    sql="""SELECT id,channel_name,channel_Description,channel_profile_pic,created_at,channel_user_id FROM channel WHERE channel_name = %s"""
-    cursor.execute(sql,(channel_name,))
-    extracted_data = cursor.fetchall()
-    
-    result = []
-    fields = ['channel_id','channel_name', 'channel_description', 'channel_profile_pic','created_at','channel_user_id']
-    result = dict(zip(fields,extracted_data[0]))
-    
-    channel_id=result['channel_id']
-    
-    sql = "INSERT INTO videos (channel_id) VALUES (%s)"
-    val = (channel_id)
-    cursor.execute(sql,val)
-    
-    sql = "INSERT INTO subscribers (channel_id) VALUES (%s)"
-    val = (channel_id)
-    cursor.execute(sql,val)
-    
-    cursor.close()
-    
-    #return http response 
-    return utils.response("Passed",response_data) 
-
